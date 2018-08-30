bc357b4fd1165181e35a7ad1a1c7db47d3e1d295
NicEastvillage <nicoesterby@gmail.com>
2018-08-30 16:40:29 +0200

    Refined DataPacket.

--- src/main/java/eastvillage/dsdragon/game/DataPacket.java
+++ src/main/java/eastvillage/dsdragon/game/DataPacket.java
@@ -1,21 +1,35 @@
 package eastvillage.dsdragon.game;
 
 import rlbot.flat.GameTickPacket;
+import rlbot.flat.PlayerInfo;
+
+import java.util.ArrayList;
 
 public class DataPacket {
 
     public final Car car;
     public final Ball ball;
-    public final int team;
     public final int playerIndex;
+    public final ArrayList<Car> allies;
+    public final ArrayList<Car> enemies;
 
-    public DataPacket(GameTickPacket request, int playerIndex) {
+    public DataPacket(GameTickPacket packet, int playerIndex) {
 
         this.playerIndex = playerIndex;
-        this.ball = new Ball(request.ball());
-
-        rlbot.flat.PlayerInfo myPlayerInfo = request.players(playerIndex);
-        this.team = myPlayerInfo.team();
-        this.car = new Car(myPlayerInfo, request.gameInfo().secondsElapsed());
+        ball = new Ball(packet.ball());
+        car = new Car(packet.players(playerIndex));
+        allies = new ArrayList<>(4);
+        enemies = new ArrayList<>(5);
+        int team = car.team.ordinal();
+        for (int i = 0; i < packet.playersLength(); i++) {
+            if (i != playerIndex) {
+                PlayerInfo player = packet.players(i);
+                if (player.team() == team) {
+                    allies.add(new Car(player));
+                } else {
+                    enemies.add(new Car(player));
+                }
+            }
+        }
     }
 }
